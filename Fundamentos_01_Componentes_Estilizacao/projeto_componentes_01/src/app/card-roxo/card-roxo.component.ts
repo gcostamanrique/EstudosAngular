42396bb693ad0398051c428645c7861cc9eb564b
Guilherme Costa Manrique <gcostamanrique@gmail.com>
2025-01-29 18:07:18 -0300

    Atualização
    Atualização com testes durante a visualização das aulas

--- Fundamentos_01_Componentes_Estilizacao/projeto_componentes_01/src/app/card-roxo/card-roxo.component.ts
+++ Fundamentos_01_Componentes_Estilizacao/projeto_componentes_01/src/app/card-roxo/card-roxo.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
+import {MatSliderModule} from '@angular/material/slider';
 
 @Component({
   selector: 'app-card-roxo',
-  imports: [CardRoxoButtonComponent],
+  imports: [CardRoxoButtonComponent, CardButtonCancelComponent, MatSliderModule],
   templateUrl: './card-roxo.component.html',
   styleUrl: './card-roxo.component.scss'
 })
--- Fundamentos_01_Componentes_Estilizacao/projeto_componentes_01/src/app/card/card.component.ts
+++ Fundamentos_01_Componentes_Estilizacao/projeto_componentes_01/src/app/card/card.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
+import {MatSliderModule} from '@angular/material/slider';
+
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string;
+  preco: number;
+}
 
 @Component({
   selector: 'app-card',
-  imports: [CardButtonComponent],
+  imports: [CardButtonComponent, CardButtonCancelComponent, MatSliderModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
+  //encapsulation: ViewEncapsulation.None,
+  //encapsulation: ViewEncapsulation.Emulated,
+  //encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class CardComponent {
-
+  plano: IPlano = {
+    infos: {
+      tipo: "Simples",
+      preco: 100,
+    },
+  };
 }
