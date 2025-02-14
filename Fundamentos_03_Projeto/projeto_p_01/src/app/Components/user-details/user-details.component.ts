f02b1c9f737bdb772291634ce8a89df5903a7c30
Guilherme Costa Manrique <gcostamanrique@gmail.com>
2025-02-14 12:36:44 -0300

    Atualização
    Atualização dos pipes criados do projeto_p_01

--- Fundamentos_03_Projeto/projeto_p_01/src/app/Components/user-details/user-details.component.ts
+++ Fundamentos_03_Projeto/projeto_p_01/src/app/Components/user-details/user-details.component.ts
@@ -4,6 +4,9 @@ import { MatDividerModule } from '@angular/material/divider';
 import { IUser } from '../../Interfaces/User/user.interface';
 import { PhonePipe } from '../../Pipes/phone.pipe';
 import { AddressPipe } from '../../Pipes/address.pipe';
+import { StatusPipe } from '../../Pipes/status.pipe';
+import { DatePipe } from '@angular/common';
+import { DashIfEmptyPipe } from '../../Pipes/dash-if-empty.pipe';
 
 @Component({
   selector: 'app-user-details',
@@ -12,6 +15,9 @@ import { AddressPipe } from '../../Pipes/address.pipe';
     MatDividerModule,
     PhonePipe,
     AddressPipe,
+    StatusPipe,
+    DatePipe,
+    DashIfEmptyPipe,
   ],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
--- Fundamentos_03_Projeto/projeto_p_01/src/app/Components/users-list/users-list.component.ts
+++ Fundamentos_03_Projeto/projeto_p_01/src/app/Components/users-list/users-list.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { UsersList } from '../../Data/users-list';
 import { IUser } from '../../Interfaces/User/user.interface';
+import { StatusPipe } from '../../Pipes/status.pipe';
+import { DatePipe } from '@angular/common';
+import { DashIfEmptyPipe } from '../../Pipes/dash-if-empty.pipe';
 
 @Component({
   selector: 'app-users-list',
   imports: [
     MatTableModule,
+    StatusPipe,
+    DatePipe,
+    DashIfEmptyPipe,
   ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
